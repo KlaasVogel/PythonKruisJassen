80f4d3852f4e33755c407f6b8f947d1ce92133f1
KlaasVogel <klaasvogel@gmail.com>
2019-12-09 19:18:17 +0100

    first stage done

--- KruisKlaverenV0.01.py
+++ KruisKlaverenV0.01.py
@@ -33,27 +33,47 @@ class Tafels(list):
     self.frame.pack(kwargs)
     self.parent=parent
     self.rondeLabel=tk.Label(self.frame,text="ronde:")
-    self.rondeLabel.grid(column=1,row=1)
-    
+    self.rondeLabel.grid(column=0,row=0)
+    self.rondenummers=[]
+
   def set(self,numTafels):
     for tafel in self:
       tafel.reset()
+    self.clear()
+    for rondenummer in self.rondenummers:
+      rondenummer.destroy()
+    self.rondenummers=[]
     if numTafels:
+      rondes=int((int(numTafels)*4-1)/3)
+      for x in range(rondes):
+        self.rondenummers.append(tk.Label(self.frame,text="{}".format(x+1)))
+        self.rondenummers[-1].grid(row=x+1,column=0)
       for x in range(int(numTafels)):
-         self.append(Tafel(self.frame,numTafels,x))
+         self.append(Tafel(self.frame,numTafels,x,rondes))
 
-class Tafel:
-  def __init__(self,parent,numTafels,tafelNummer):
+class Tafel(list):
+  def __init__(self,parent,numTafels,tafelNummer,rondes):
+    self.label=tk.Label(parent, text="Tafel {}".format(tafelNummer+1))
+    self.label.grid(row=0,column=int(1+tafelNummer*4),padx=(25,0))
+    for x in range(rondes):
+      self.append(Ronde(parent,numTafels))
+
+  def reset(self):
+    for ronde in self:
+      ronde.reset()
+    self.clear()
+    self.label.destroy()
+
+class Ronde:
+  def __init__(self,parent,numTafels):
     self.parent=parent
-    self.tafelNummer=tafelNummer
-    self.vakjes=[]
+    self.vlakken=[]
     self.opties=[]
     for x in range(int(numTafels)*4):
       self.opties.append(x+1)
-    print("set: ", tafelNummer,self.opties)
 
   def reset(self):
-    print("reset: ",self.tafelNummer)
+    pass
 
 
 if __name__ == "__main__":
