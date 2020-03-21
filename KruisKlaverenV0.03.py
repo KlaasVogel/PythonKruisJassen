c887b5080f83ceeb50566aac917da2175371c16c
KlaasVogel <klaasvogel@gmail.com>
2020-03-21 23:19:03 +0100

    long version no result for 5 tables yet

--- KruisKlaverenV0.03.py
+++ KruisKlaverenV0.03.py
@@ -4,12 +4,15 @@ import sys
 import logging
 from logger import MyLogger
 from math import ceil
+import os
 
 #Replace stdout with logging to file at INFO level
 #sys.stdout = MyLogger("SYSTEM_OUTPUT", logging.INFO)
 # Replace stderr with logging to file at ERROR level
 #sys.stderr = MyLogger("SYSTEM_ERROR", logging.ERROR)
 
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 #create a new grid
 #input number of tables
@@ -17,21 +20,46 @@ def createGrid(numTables:int) :
     global grid
     numPlayers=numTables*4
     numRounds=(numPlayers-1)//3
-    if (numPlayers-1)%3 :
-        numRounds-=2
     grid=np.zeros((numRounds,numTables,4),dtype=int)
-    print("players: {} , rounds: {}".format(numPlayers,numRounds))
+    #fill first round
+    grid[0]=np.array(np.arange(1,numPlayers+1)).reshape(numTables,4)
+    #get blocked
+    numBlocks=numPlayers-(1+numRounds*3)
+    blocked=(1+numPlayers//2-1)%numPlayers+1 if numBlocks==1 else 0
+    """
+    for i in range(1,5):
+        if i < numRounds :
+            for y in range(1,numRounds):
+                grid[y][i-1][0]=i
+    """
+    for y in range(1,numRounds) :
+        for x in range(numTables) :
+            if x<4 :
+                grid[y][x][0]=x+1
+                if blocked and y==x:
+                    grid[y][x][2]=blocked
+            elif y>=4 :
+                grid[y][x][2]=blocked
 
 
 def possible(y,x,n) :
     global grid
-    numRounds,numTables,dummy=grid.shape
-    numPlayers=numTables*4
+    numRounds,numTables,tableSize=grid.shape
+    numPlayers=numTables*tableSize
+
+    blocked=[]
+    numBlocks=numPlayers-(1+numRounds*3)
+    if numBlocks :
+        if numBlocks == 1 :
+            blocked=[(n+numPlayers//2-1)%numPlayers+1]
+        else:
+            blocked.append((n+3)%numPlayers+1) #plus four
+            blocked.append((n+numPlayers-5)%numPlayers+1) #minus four
     #check if table isn't full:
     if all(grid[y][x]) :
         return False
-    #check if new player has highest number
-    if any(player>n for player in grid[y][x]):
+    #check if new player has highest number or blocked player in team
+    if any(player in blocked for player in grid[y][x]):
         return False
     #check if player isn't  already playing in this round:
     if any(n in table for table in grid[y]) :
@@ -45,29 +73,33 @@ def possible(y,x,n) :
     return True
 
 def solve():
-    global grid
-    numRounds,numTables,dummy=grid.shape
-    numPlayers=numTables*4
-    for y in range(numRounds):
-        for x in range(numTables):
-            if not all(grid[y][x]):
-                for n in range(1,numPlayers+1) :
+    global grid, count
+    numRounds,numTables,tableSize=grid.shape
+    numPlayers=numTables*tableSize
+    for y in range(1,numRounds) :
+        for n in range(1,numPlayers+1) :
+            if not any(n in grid[y][x] for x in range(numTables)) :
+                #player n is not placed this round:
+                for x in range(numTables) :
                     if possible(y, x, n) :
                         #add player:
                         for i,player in enumerate(grid[y][x]) :
                             if not player:
                                 grid[y][x][i]=n
+                                count+=1
+                                if not count%2000 or (y+3 > numRounds and not n%(numPlayers-1)) or n==numPlayers:
+                                    cls()
+                                    print(grid)
                                 break
-                        solve()
-                        #reset
-                        for i,player in enumerate(grid[y][x]) :
-                            if player==n:
-                                grid[y][x][i]=0
-                return
-            if y==2:
-                print(grid)
-    print(grid)
-    input("more?")
+                        if not solve():
+                            #reset
+                            for i,player in enumerate(grid[y][x]) :
+                                if player==n:
+                                    grid[y][x][i]=0
+                        else:
+                            return True
+                return False
+    return True
 
 
 #function to create nice gradient of colors for players
@@ -87,7 +119,10 @@ def colorPicker(min,max,value):
 
 numTables=5
 createGrid(numTables)
+count=0
 solve()
+print("whoooot!")
+print(grid)
 
 
 
