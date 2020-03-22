c3a9914dc7f0b86d253b6810f469311464a14eea
KlaasVogel <klaasvogel@gmail.com>
2020-03-22 23:50:35 +0100

    getting percentage visable

--- KruisKlaverenV0.03.py
+++ KruisKlaverenV0.03.py
@@ -4,6 +4,7 @@ import sys
 import logging
 from logger import MyLogger
 from math import ceil
+from time import sleep
 import os
 
 #Replace stdout with logging to file at INFO level
@@ -30,6 +31,9 @@ class Grid(np.ndarray):
                         self.blocks[n]=[(n+self.numPlayers//2-1)%self.numPlayers+1]
                     else:
                         self.blocks[n]=[(n+3)%self.numPlayers+1,(n+self.numPlayers-5)%self.numPlayers+1] #plus four and minus four
+            self.scores={}
+            for i in range(1,self.numPlayers+1):
+                self.scores[i]=0
         except Exception as e:
             print('ERROR bij het maken van Grid')
             print(e)
@@ -46,6 +50,10 @@ class Grid(np.ndarray):
     def __array_finalize__(self, obj):
         if obj is None: return
         self.info = getattr(obj, 'info', None)
+
+    def process(self):
+        pass
+
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
@@ -66,13 +74,25 @@ def createGrid(numTables:int) :
         for x in range(numTables) :
             if x<4 :
                 grid[y][x][0]=x+1
+                if y==x :
+                    grid[y][x][2]=blocked
+            elif y>3:
+                grid[y][x][2]=blocked
+
     return Grid(grid)
 
 #need to check if all combos are present / available
-combo_logger=MyLogger("Possibility Check", logging.DEBUG)
+com_logger=MyLogger("Combo Check", logging.DEBUG)
 def check_combo() :
     global grid, combo_logger
-
+    for n in range(1,grid.numPlayers) :
+        for m in range(n+1,grid.numPlayers+1) :
+            if not (n in grid.blocks and m in grid.blocks[n]):
+                if not any((n in grid[y][x] or possible(y,x,n)) and (m in grid[y][x] or possible(y,x,m)) for y in range(grid.numRounds) for x in range(grid.numTables)):
+                    com_logger.debug("Combo {} and {} not possible!".format(n,m))
+                    return False
+    #com_logger.debug("All combo's are possible")
+    return True
 
 #check if player can be placed.
 pos_logger=MyLogger("Possibility Check", logging.INFO)
@@ -84,7 +104,8 @@ def possible(y,x,n) :
         pos_logger.debug("table full")
         return False
     #check if new player is blocked by player in team
-    if any(player in grid.blocks[n] for player in grid[y][x]):
+
+    if n in grid.blocks and any(player in grid.blocks[n] for player in grid[y][x]):
         pos_logger.debug("player {} blocked by".format(n))
         return False
     #check if player isn't  already playing in this round:
@@ -102,16 +123,18 @@ def possible(y,x,n) :
 
 def solve():
     global grid, count, logger
+    maxlevel=1
     count+=1
     if count == 1:
         logger = MyLogger("SOLVE", logging.INFO)
     logger.debug("START SOLVE: {}".format(count))
     level=int(np.log10(count))
-    if level > 3 :
-        level=3
+    if level > maxlevel :
+        level=maxlevel
     if not count%(10**level):
         cls()
         print("count: {} - level: {}".format(count,level))
+        print(grid.scores)
         print(grid)
     numRounds,numTables,tableSize=grid.shape
     numPlayers=numTables*tableSize
@@ -120,6 +143,8 @@ def solve():
         for y in range(1,numRounds) :
             logger.debug('round: {}'.format(y))
             if not any(n in grid[y][x] for x in range(numTables)) :
+                if not grid.scores[n]:
+                    grid.scores[n]=y
                 logger.debug('empty round')
                 #player n is not placed this round:
                 for x in range(numTables) :
@@ -131,17 +156,22 @@ def solve():
                                 grid[y][x][i]=n
                                 break
                         logger.debug('SOLVE NEXT LEVEL')
-                        if not solve() :
+                        if not (check_combo() and solve()):
                         #reset
                             logger.debug("reset")
                             for i,player in enumerate(grid[y][x]) :
                                 if player==n:
                                     grid[y][x][i]=0
                         else:
+                            grid.scores[n]=x+1
                             logger.debug('return True 1')
                             return True
                 logger.debug('return False 1')
                 return False
+            else:
+                if not grid.scores[n] :
+                    grid.scores[n]=grid.numTables
+                logger.debug("{} is done!".format(n))
     logger.debug('RETURN FINAL TRUE')
     return True
 
@@ -165,8 +195,8 @@ numTables=5
 grid=createGrid(numTables)
 print('test - numblocks {}'.format(grid.blocks))
 print(grid)
+print(grid.scores)
 count=0
-
 
 if solve() :
     print("whoooot!")
